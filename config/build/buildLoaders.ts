1c54a1c02404433bcde8b6cbd40f21c7646545d2
Denis <denis.pedan.92@mail.ru>
2024-03-13 22:27:25 +0500

    added more components, logic and structure

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,10 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "./types/config";
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+    const svgLoader = {
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    }
 
     const cssLoaders = {
         test: /\.s[ac]ss$/i,
@@ -29,7 +33,18 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
             exclude: /node_modules/,
           }
 
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      }
+
     return [
+        fileLoader,
+        svgLoader,
         typescriptLoader,
         cssLoaders
         ]
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,18 +4,21 @@ import {Link, Route, Routes} from "react-router-dom";
 import {useTheme} from "app/providers/ThemeProvider/lib/useTheme";
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
 
 
 const App = () => {
 
-    const {theme, toggleTheme} = useTheme();
+    const {theme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная страница</Link>
-            <Link to={'/about'}>О сайте</Link>
-            <AppRouter/>
+            <Navbar/>
+            <div className="content-page">
+                {/*<Sidebar/>*/}
+                <AppRouter/>
+            </div>
         </div>
     );
 };
